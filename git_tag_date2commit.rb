81d5166c0ed99267ff5a592b691afa5e6a3714f2
rfauglas <ronan.fauglas@akeneo.com>
2021-03-11 15:21:12 +0100

    add date

--- git_tag_date2commit.rb
+++ git_tag_date2commit.rb
@@ -10,7 +10,7 @@ CSV.parse(File.read(ARGV[0])).each do |tag,date|
 
   git_cmd = "git rev-list -1 --before=\"#{date}\" \"remotes/origin/#{branch}\""
   commit_id = `#{git_cmd}`.chop
-  curl_cmd= "curl -H \"X-API-Key: #{ENV['ATHENIAN_TOKEN']}\" https://api.athenian.co/v1/events/releases --data '[{\"repository\": \"github.com/akeneo/pim-community-dev\", \"commit\": \"#{commit_id}\"}]'"
+  curl_cmd= "curl -H \"X-API-Key: #{ENV['ATHENIAN_TOKEN']}\" https://api.athenian.co/v1/events/releases --data '[{\"repository\": \"github.com/akeneo/pim-community-dev\", \"commit\": \"#{commit_id}\", \"published_at\":\"#{date}\"}]'"
 
   puts curl_cmd
   puts `#{curl_cmd}`
